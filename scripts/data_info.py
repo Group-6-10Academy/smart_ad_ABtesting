abe06181133c8d7555920625fe9d671c05ae0c4e
ekubay <axuTec14@gmail.com>
2022-08-30 21:32:07 +0300

    detaild info

--- scripts/data_info.py
+++ scripts/data_info.py
@@ -4,10 +4,17 @@ import pandas as pd
 class DataInfo:
     def __init__(self, df):
         self.df = df.copy()
-# shape of the dataframe
+    # shape of the dataframe
     def shape_df(self):
          '''
          Display number of rows and columns in the given Dataframe
          '''
          print(f"Dataframe contains {self.df.shape[0]} rows and {self.df.shape[1]} columns")
          #return (self.df.shape[0],self.df.shape[1])
+     # info
+    def detail_info(self):
+        '''
+        Display detail Dataframe info
+        '''
+        print(self.df.info())
+    # satistical description
