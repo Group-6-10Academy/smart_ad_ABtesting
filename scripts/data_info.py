b230c775cbd1afded1edb7e5f9fac73c72ec77e5
ekubay <axuTec14@gmail.com>
2022-08-30 21:30:47 +0300

    info about shape

--- scripts/data_info.py
+++ scripts/data_info.py
@@ -4,3 +4,10 @@ import pandas as pd
 class DataInfo:
     def __init__(self, df):
         self.df = df.copy()
+# shape of the dataframe
+    def shape_df(self):
+         '''
+         Display number of rows and columns in the given Dataframe
+         '''
+         print(f"Dataframe contains {self.df.shape[0]} rows and {self.df.shape[1]} columns")
+         #return (self.df.shape[0],self.df.shape[1])
