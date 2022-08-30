62128e40bed3d44fa5522262d43d00ec5d96aa12
akrobi <gwangui00000@gmail.com>
2022-08-30 23:44:50 +0300

    added rotation in xticks in plot_scatter

--- scripts/VisualisationPlots.py
+++ scripts/VisualisationPlots.py
@@ -68,7 +68,7 @@ class VisualiseDf:
         plt.figure(figsize=(12, 7))
         sns.scatterplot(data = df, x=x_col, y=y_col, hue=hue, style=style)
         plt.title(title, size=20)
-        plt.xticks(fontsize=14)
+        plt.xticks(rotation=75, fontsize=14)
         plt.yticks( fontsize=14)
         plt.show()
 
