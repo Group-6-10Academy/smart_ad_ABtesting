f37135d0b9beaa8ba4081d7ae47b0d7ba1cd6d8b
ekubay <axuTec14@gmail.com>
2022-09-02 07:59:48 +0300

    checking mlflow and dvc

--- mlflow_train.py
+++ mlflow_train.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error , mean_absolute_error , r2_score
 from sklearn.model_selection import train_test_split
-from sklearn import ElasticNet
+from sklearn.linear_model import ElasticNet
 from urllib.parse import urlparse
 import mlflow
 #import mlflow.sklearn
